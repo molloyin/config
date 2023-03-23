f5e5aea03eaabae3e1c98d2cac31522be3f1e594
Matthew Molloy <83689572+molloyin@users.noreply.github.com>
2023-03-23 16:59:37 +1300

    LSP and much more!

--- nvim/lua/molloyin/packer.lua
+++ nvim/lua/molloyin/packer.lua
@@ -8,6 +8,26 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                suggestion = { enabled = false },
+                panel = { enabled = false } 
+            })
+        end,
+    }
+
+    use {
+        "zbirenbaum/copilot-cmp",
+        after = { "copilot.lua" },
+        config = function ()
+            require("copilot_cmp").setup()
+        end
+    }
+
+    use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
@@ -44,9 +64,8 @@ return require('packer').startup(function(use)
     }
 
     use("tpope/vim-fugitive")
-    use("github/copilot.vim")
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use("mbbill/undotree")
 
-
 end)
+
